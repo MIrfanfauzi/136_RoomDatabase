317dad504988f6d5d90c72dcdd8018cd60e81f31
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-11-27 16:51:16 +0700

    membuat model DetailSiswa dan fun toSiswa

---
+++ app/src/main/java/com/example/myroomsatu/viewmodel/EntryViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.myroomsatu.viewmodel
+
+import com.example.myroomsatu.room.Siswa
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
+
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon,
+)
