6af49addbfbd005c83e8e12cb9139cbdcf31cfbe
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-11-27 21:01:26 +0700

    membuat fungsi ekstensi aplikasiSiswa untuk akses instance aplikasi pada viewmodel

---
+++ app/src/main/java/com/example/myroomsatu/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.myroomsatu.viewmodel.provider
+
+import android.app.Application
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.myroomsatu.repositori.AplikasiSiswa
+import com.example.myroomsatu.viewmodel.EntryViewModel
+import com.example.myroomsatu.viewmodel.HomeViewModel
+
+    fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
