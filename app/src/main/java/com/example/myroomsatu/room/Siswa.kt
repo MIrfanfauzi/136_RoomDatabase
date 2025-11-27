e4fbc1c687042feefb3d6471e787ad4edcbe90cb
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-11-27 15:54:01 +0700

    membuat data class siswa

---
+++ app/src/main/java/com/example/myroomsatu/room/Siswa.kt
@@ -0,0 +1,13 @@
+package com.example.myroomsatu.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tbl_siswa")
+data class Siswa(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val nama: String,
+    val alamat: String,
+    val telpon: String
+)
