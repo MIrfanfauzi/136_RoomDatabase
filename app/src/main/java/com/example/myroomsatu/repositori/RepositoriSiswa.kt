aa211057212af25e3946e524917467224c879ff0
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-11-27 16:19:54 +0700

    membuat interface RepositoriSiswa dengan operasi dasar

---
+++ app/src/main/java/com/example/myroomsatu/repositori/RepositoriSiswa.kt
@@ -0,0 +1,13 @@
+package com.example.myroomsatu.repositori
+
+import com.example.myroomsatu.room.Siswa
+import com.example.myroomsatu.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+}
+
+
+
