edd2e1422607f6a4c11f839471e67953ae2d63df
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-11-27 19:35:52 +0700

    membuat entry viewmodel dan validasi input

--- app/src/main/java/com/example/myroomsatu/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/myroomsatu/viewmodel/EntryViewModel.kt
@@ -1,29 +1,47 @@
 package com.example.myroomsatu.viewmodel
 
+import android.R.attr.data
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.myroomsatu.repositori.RepositoriSiswa
 import com.example.myroomsatu.room.Siswa
 
-data class DetailSiswa(
-    val id: Int = 0,
-    val nama: String = "",
-    val alamat: String = "",
-    val telpon: String = "",
-)
 
-fun DetailSiswa.toSiswa(): Siswa = Siswa(
-    id = id,
-    nama = nama,
-    alamat = alamat,
-    telpon = telpon,
-)
+class EntryViewModel (private val repositoriSiswa: RepositoriSiswa) : ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
 
-fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
-    detailSiswa = this.toDetailSiswa(),
-    isEntryValid = isEntryValid
-)
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(receiver = uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
 
-fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
-    id = id,
-    nama = nama,
-    alamat = alamat,
-    telpon = telpon,
-)
+    
+
+    data class DetailSiswa(
+        val id: Int = 0,
+        val nama: String = "",
+        val alamat: String = "",
+        val telpon: String = "",
+    )
+
+    fun DetailSiswa.toSiswa(): Siswa = Siswa(
+        id = id,
+        nama = nama,
+        alamat = alamat,
+        telpon = telpon,
+    )
+
+    fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+        detailSiswa = this.toDetailSiswa(),
+        isEntryValid = isEntryValid
+    )
+
+    fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+        id = id,
+        nama = nama,
+        alamat = alamat,
+        telpon = telpon,
+    )
+}
