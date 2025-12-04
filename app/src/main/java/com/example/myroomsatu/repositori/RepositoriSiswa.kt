2713f1dda80991c1675c8019c521c1dbcac3bb64
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-12-04 15:20:45 +0700

    menambahkan fungsi get dan delete siswa pada repositori

--- app/src/main/java/com/example/myroomsatu/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myroomsatu/repositori/RepositoriSiswa.kt
@@ -7,6 +7,8 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
+    suspend fun getSiswaStream(id: Int): Flow<Siswa?>
+    suspend fun deleteSiswa(siswa: Siswa)
 }
 
 class OfflineRepositoriSiswa(
@@ -14,5 +16,7 @@ class OfflineRepositoriSiswa(
 ) : RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+    override suspend fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
 }
 
